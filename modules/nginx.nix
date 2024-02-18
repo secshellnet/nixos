e86a02f9a0dd70068f26135258c30deb7552fd0d
Nico Felbinger <nico@felbinger.eu>
2024-02-18 15:59:19 +0100

    Add modsecurity web application firewall with owasp core ruleset to nginx

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -1,5 +1,6 @@
 { lib
 , config
+, pkgs
 , ...
 }: {
   options.secshell.nginx = {
@@ -11,11 +12,81 @@
       type = lib.types.str;
       default = "acme@secshell.net";
     };
+    modsecurity = {
+      # TODO After testing has been complete this should be enabled by default at least
+      #      with SecRuleEngine DetectionOnly to prevent blocking of legitimate services
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
+      detectionOnly = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
+      paranoiaLevel = lib.mkOption {
+        type = lib.types.int;
+        default = 1;
+      };
+      preFile = lib.mkOption {
+        type = lib.types.path;
+        default = pkgs.writeText "modsecurity_pre.conf" "";
+      };
+      postFile = lib.mkOption {
+        type = lib.types.path;
+        default = pkgs.writeText "modsecurity_post.conf" "";
+      };
+    };
   };
   config = lib.mkIf config.services.nginx.enable {
     sops.secrets."cloudflareToken".owner = "root";
 
-    services.nginx = {
+    services.nginx = let
+      modsecurity_crs = pkgs.fetchFromGitHub {
+        owner = "coreruleset";
+        repo = "coreruleset";
+        rev = "v4.0.0";
+        sha256 = "TErAhbD77Oa2IauqBnLD+lMk4aI0hWgLb4CcCjqQRdQ=";
+      };
+      modsecurity_conf = pkgs.writeText "modsecurity.conf" ''
+        # TODO Logging doesn't work with DetectionOnly mode (journalctl -f -u nginx shows no logs)
+        SecRuleEngine ${if (config.secshell.nginx.modsecurity.detectionOnly) then "DetectionOnly" else "On"}
+
+        SecDefaultAction "phase:1,log,auditlog,pass"
+        SecDefaultAction "phase:2,log,auditlog,pass"
+
+        Include ${config.secshell.nginx.modsecurity.preFile}
+
+        SecAction \
+            "id:900000,\
+            phase:1,\
+            pass,\
+            t:none,\
+            nolog,\
+            setvar:tx.blocking_paranoia_level=${toString config.secshell.nginx.modsecurity.paranoiaLevel}"
+
+        SecAction \
+            "id:900010,\
+            phase:1,\
+            pass,\
+            t:none,\
+            nolog,\
+            setvar:tx.enforce_bodyproc_urlencoded=1"
+
+        SecCollectionTimeout 600
+
+        SecAction \
+            "id:900990,\
+            phase:1,\
+            pass,\
+            t:none,\
+            nolog,\
+            setvar:tx.crs_setup_version=400"
+
+        Include ${modsecurity_crs}/rules/*.conf
+
+        Include ${config.secshell.nginx.modsecurity.postFile}
+      '';
+    in {
       enableReload = true;
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
@@ -23,6 +94,12 @@
       recommendedZstdSettings = true;
       recommendedGzipSettings = true;
       recommendedBrotliSettings = true;
+
+      additionalModules = lib.mkIf config.secshell.nginx.modsecurity.enable [ pkgs.nginxModules.modsecurity ];
+      appendHttpConfig = lib.mkIf config.secshell.nginx.modsecurity.enable ''
+        modsecurity on;
+        modsecurity_rules_file ${modsecurity_conf};
+      '';
     };
     networking.firewall.allowedTCPPorts = [ 80 443 ];
 
