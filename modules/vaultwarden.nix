90fd32faccf3a6dd9936a41900feecaa75c71ba2
Nico Felbinger <nico@felbinger.eu>
2024-05-10 23:40:52 +0200

    vaultwarden: breaking changes: split env secret into multiple keys

--- modules/vaultwarden.nix
+++ modules/vaultwarden.nix
@@ -35,9 +35,50 @@
       type = lib.types.bool;
       default = true;
     };
+    database = {
+      hostname = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+      };
+      username = lib.mkOption {
+        type = lib.types.str;
+        default = "vaultwarden";
+      };
+      name = lib.mkOption {
+        type = lib.types.str;
+        default = "vaultwarden";
+      };
+    };
   };
   config = lib.mkIf config.secshell.vaultwarden.enable {
-    sops.secrets."vaultwarden/env" = {};
+    sops = {
+      secrets = {
+        "vaultwarden/adminToken" = {};
+        "vaultwarden/hibpApiKey" = {};
+      } // (lib.optionalAttrs (! config.secshell.vaultwarden.useLocalDatabase) {
+        "vaultwarden/databasePassword" = {};
+      }) // (lib.optionalAttrs (
+          config.secshell.vaultwarden.smtp.hostname != null
+            && config.secshell.vaultwarden.smtp.from != null
+        ) {
+        "vaultwarden/smtpUsername" = {};
+        "vaultwarden/smtpPassword" = {};
+      });
+
+      templates."vaultwarden/env".content = let
+        databaseUsername = config.secshell.vaultwarden.database.username;
+        databasePassword = config.sops.placeholder."vaultwarden/databasePassword";
+        databaseHostname = config.secshell.vaultwarden.database.hostname;
+        databaseName = config.secshell.vaultwarden.database.name;
+        databaseUri = "postgresql://${databaseUsername}:${databasePassword}@${databaseHostname}/${databaseName}";
+      in ''
+        ADMIN_TOKEN=${config.sops.placeholder."vaultwarden/adminToken"}
+        HIBP_API_KEY=${config.sops.placeholder."vaultwarden/hibpApiKey"}
+        ${lib.optionalString (! config.secshell.vaultwarden.useLocalDatabase) "DATABASE_URL=${databaseUri}"}
+        ${lib.optionalString (config.secshell.vaultwarden.smtp.hostname != null && config.secshell.vaultwarden.smtp.from != null) "SMTP_USERNAME=${config.sops.placeholder."vaultwarden/smtpUsername"}"}
+        ${lib.optionalString (config.secshell.vaultwarden.smtp.hostname != null && config.secshell.vaultwarden.smtp.from != null) "SMTP_PASSWORD=${config.sops.placeholder."vaultwarden/smtpPassword"}"}
+      '';
+    };
 
     services.postgresql = lib.mkIf config.secshell.vaultwarden.useLocalDatabase {
       enable = true;
@@ -46,7 +87,7 @@
 
     services.vaultwarden = {
       enable = true;
-      environmentFile = config.sops.secrets."vaultwarden/env".path;
+      environmentFile = config.sops.templates."vaultwarden/env".path;
       config = {
         ROCKET_ADDRESS = "127.0.0.1";
         ROCKET_PORT = config.secshell.vaultwarden.internal_port;
