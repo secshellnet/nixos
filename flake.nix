37351e66acbd4ba198265e25f39a0fa00f1dfb79
Nico Felbinger <nico@felbinger.eu>
2024-06-22 21:14:13 +0200

    Restructure flake.nix

--- flake.nix
+++ flake.nix
@@ -3,23 +3,26 @@
     nixosModules = {
       default = {
         imports = [
+          # Web Applications
+          ./modules/gitea.nix
+          ./modules/graylog.nix
           ./modules/hedgedoc.nix
+          ./modules/keycloak.nix
+          ./modules/matrix/default.nix
+          ./modules/monitoring/default.nix
+          ./modules/netbox.nix
+          ./modules/nexus.nix
+          ./modules/paperless.nix
           ./modules/simple-upload.nix
+          ./modules/unifi.nix
+          ./modules/vaultwarden.nix
+          ./modules/woodpecker.nix
+
+          # Support Modules
+          ./modules/containers.nix
           ./modules/nginx.nix
           ./modules/postgres.nix
-          ./modules/containers.nix
-          ./modules/gitea.nix
-          ./modules/woodpecker.nix
-          ./modules/vaultwarden.nix
-          ./modules/netbox.nix
-          ./modules/keycloak.nix
-          ./modules/paperless.nix
-          ./modules/monitoring/default.nix
-          ./modules/matrix/default.nix
-          ./modules/unifi.nix
           ./modules/user.nix
-          ./modules/nexus.nix
-          ./modules/graylog.nix
         ];
       };
     };
