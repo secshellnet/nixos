616234a895caad51e385e3e8bbf64de69e349405
Nico Felbinger <nico@felbinger.eu>
2024-07-27 16:59:05 +0200

    bind: Add statistics-channels and prometheus exporter which is enabled by default

--- modules/bind.nix
+++ modules/bind.nix
@@ -2,7 +2,12 @@
 , pkgs
 , lib
 , ...
-}: {
+}: let
+  mkDisableOption = name: lib.mkEnableOption name // {
+    default = true;
+    example = false;
+  };
+in {
   options.secshell.bind = {
     enable = lib.mkEnableOption "bind";
     zones = lib.mkOption {
@@ -13,6 +18,7 @@
       type = lib.types.str;
       default = "Authorative Name Server";
     };
+    exporter.enable = mkDisableOption "bind_exporter";
   };
   config = lib.mkIf config.secshell.bind.enable {
     services.bind = {
@@ -28,6 +34,12 @@
         # track stats on zones
         zone-statistics yes;
       '';
+      extraConfig = lib.mkIf config.secshell.bind.exporter.enable ''
+        statistics-channels {
+          inet 127.0.0.1 port 8053 allow { 127.0.0.1; };
+        };
+      '';
     };
+    services.prometheus.exporters.bind.enable = config.secshell.bind.exporter.enable;
   };
 }
