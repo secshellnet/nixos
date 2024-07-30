3cb1876a2829e861fe401a2abf31ea3c1b7559be
Nico Felbinger <nico@felbinger.eu>
2024-07-30 15:09:40 +0200

    hardening: Add fail2ban

--- modules/hardening.nix
+++ modules/hardening.nix
@@ -45,8 +45,8 @@ in {
       };
     };
 
-    services.openssh = {
-      settings = {
+    services = {
+      openssh.settings = {
         PermitRootLogin = lib.mkDefault "no";
         PasswordAuthentication = lib.mkDefault false;
         AllowTcpForwarding = lib.mkDefault false;
@@ -58,6 +58,12 @@ in {
         LoginGraceTime = lib.mkDefault 30;
         AllowUsers = lib.mkDefault ([ "root" ] ++ config.secshell.users);
         MaxStartups = lib.mkDefault "10:30:60";
+      };
+
+      fail2ban = lib.mkIf config.services.openssh.enable {
+        enable = true;
+        maxretry = 10;
+        bantime = "24h";
       };
     };
   };
