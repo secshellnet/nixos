75c1ce4f791b3367141bc402d80ce0c7d4255438
Nico Felbinger <nico@felbinger.eu>
2024-09-11 00:55:16 +0200

    bind/docs: init

--- modules/bind.nix
+++ modules/bind.nix
@@ -16,41 +16,35 @@ in
 {
   options.secshell.bind = {
     enable = lib.mkEnableOption "bind";
-    zones = lib.mkOption {
-      type = lib.types.attrs;
-      default = { };
-    };
     versionText = lib.mkOption {
       type = lib.types.str;
       default = "Authorative Name Server";
     };
     exporter.enable = mkDisableOption "bind_exporter";
-    forwarders = lib.mkOption {
-      default = [ ];
-      type = lib.types.listOf lib.types.str;
-      description = ''
-        List of servers we should forward requests to.
-      '';
-    };
   };
   config = lib.mkIf config.secshell.bind.enable {
     services.bind = {
       enable = true;
-      forwarders = config.secshell.bind.forwarders;
-      zones = config.secshell.bind.zones;
-      extraOptions = ''
-        recursion ${if (builtins.length config.secshell.bind.forwarders) == 0 then "no" else "yes" };
+      extraOptions =
+        let
+          # whether bind should be configured as forwarder
+          forward = (builtins.length config.services.bind.forwarders) > 0;
+        in
+        ''
+          recursion ${if forward then "yes" else "no"};
+          ${lib.optionalString forward ''
+            auth-nxdomain no;
+            dnssec-validation no;
+          ''}
 
-        ${if (builtins.length config.secshell.bind.forwarders) == 0 then "" else "auth-nxdomain no;" }
+          # obscure bind9 chaos version queries
+          version "${config.secshell.bind.versionText}";
 
-        ${if (builtins.length config.secshell.bind.forwarders) == 0 then "" else "dnssec-validation no;" }
-
-        # obscure bind9 chaos version queries
-        version "${config.secshell.bind.versionText}";
-
-        # track stats on zones
-        zone-statistics yes;
-      '';
+          ${lib.optionalString config.secshell.bind.exporter.enable ''
+            # track stats on zones
+            zone-statistics yes;
+          ''}
+        '';
       extraConfig = lib.mkIf config.secshell.bind.exporter.enable ''
         statistics-channels {
           inet 127.0.0.1 port 8053 allow { 127.0.0.1; };
