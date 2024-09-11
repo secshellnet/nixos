f1079eb155d5c4a44e6fd26e6b1fdb01cbe4b8e3
Nico Felbinger <nico@felbinger.eu>
2024-09-11 03:54:25 +0200

    hardening: improve

--- modules/hardening.nix
+++ modules/hardening.nix
@@ -23,6 +23,11 @@ in
     nix.settings.allowed-users = [ "@wheel" ];
     security.sudo.execWheelOnly = true;
 
+    # weird logrotate issue during config check
+    # cannot find name for group ID 30000
+    # https://discourse.nixos.org/t/logrotate-config-fails-due-to-missing-group-30000/28501
+    security.unprivilegedUsernsClone = true;
+
     boot = {
       extraModprobeConfig =
         let
@@ -67,11 +72,6 @@ in
         "net.ipv6.conf.all.forwarding" = lib.mkDefault 0;
         "net.ipv6.conf.all.accept_ra" = lib.mkDefault 0;
         "net.ipv6.conf.default.accept_ra" = lib.mkDefault 0;
-
-        # weird logrotate issue during config check
-        # cannot find name for group ID 30000
-        # https://discourse.nixos.org/t/logrotate-config-fails-due-to-missing-group-30000/28501
-        "kernel.unprivileged_userns_clone" = lib.mkDefault 1;
       };
     };
 
