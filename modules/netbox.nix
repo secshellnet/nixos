00fbaf44b603a14fd866c81808a4a3487221c946
Nico Felbinger <nico@felbinger.eu>
2025-02-27 16:17:02 +0100

    netbox/plugins-interface-sync: update to 4.1.6 to test #385464

--- modules/netbox.nix
+++ modules/netbox.nix
@@ -158,7 +158,10 @@
             (lib.mkIf config.secshell.netbox.plugin.qrcode ps.netbox-qrcode)
             (lib.mkIf config.secshell.netbox.plugin.topologyViews ps.netbox-topology-views)
             #(lib.mkIf config.secshell.netbox.plugin.proxbox plugins.netbox-proxbox)
-            (lib.mkIf config.secshell.netbox.plugin.interface-synchronization ps.netbox-interface-synchronization)
+            (lib.mkIf config.secshell.netbox.plugin.interface-synchronization (ps.netbox-interface-synchronization.overridePythonAttrs (previous: {
+              version = "4.1.6";
+              src = previous.src.override { tag = "4.1.6"; hash = "sha256-scsNigSqKWeauAyIDxDzwbgtl3rM5CGBCCmVj/98w84="; };
+            })))
             (lib.mkIf config.secshell.netbox.plugin.dns ps.netbox-dns)
             (lib.mkIf config.secshell.netbox.plugin.napalm (
               ps.netbox-napalm-plugin.overridePythonAttrs (previous: {
