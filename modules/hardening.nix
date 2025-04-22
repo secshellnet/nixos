576545b84d69579e29bab79fda4a2d4ba79a48cf
Nico Felbinger <nico@felbinger.eu>
2025-04-22 08:51:07 +0200

    hardening: strengthen password policy

--- modules/hardening.nix
+++ modules/hardening.nix
@@ -137,14 +137,21 @@ in
         # order BEFORE pam_unix.so
         order = config.security.pam.services.passwd.rules.password.unix.order - 10;
         settings = {
-          retry = 3;
-          minlen = 8;
-          difok = 6;
-          dcredit = -1;
-          ucredit = 1;
-          ocredit = -1;
-          lcredit = 1;
-          enforce_for_root = true;
+          minlen = lib.mkDefault 12;
+
+          # at least 6 characters must differ from the old password
+          difok = lib.mkDefault 6;
+
+          # required characters (at least one digit, lowercase and uppercase letter)
+          dcredit = lib.mkDefault (-1);
+          lcredit = lib.mkDefault (-1);
+          ucredit = lib.mkDefault (-1);
+          ocredit = lib.mkDefault 1;
+
+          # no more than 3 repeated characters in a row
+          maxrepeat = lib.mkDefault 3;
+
+          enforce_for_root = lib.mkDefault true;
         };
       };
       unix = {
