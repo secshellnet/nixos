d35c0228514d4dd66a2923aa29a9c5c8a4a6fe4a
Nico Felbinger <nico@felbinger.eu>
2025-05-29 10:13:09 +0200

    user: use nix variable to set fqdn in bashrc

--- modules/user.nix
+++ modules/user.nix
@@ -52,9 +52,9 @@
     # Show fqdn instead of short hostname in ps1
     environment.etc."bashrc.local".text = ''
       if [ "$EUID" -eq 0 ]; then
-        export PS1='\n\[\033[1;31m\][\[\e]0;\u@'$(hostname -f)': \w\a\]\u@'$(hostname -f)':\w]\$\[\033[0m\] '
+        export PS1='\n\[\033[1;31m\][\[\e]0;\u@'${config.networking.fqdnOrHostName}': \w\a\]\u@'${config.networking.fqdnOrHostName}':\w]\$\[\033[0m\] '
       else
-        export PS1='\[\033[01;32m\]\u@'$(hostname -f)'\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$ '
+        export PS1='\[\033[01;32m\]\u@'${config.networking.fqdnOrHostName}'\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$ '
       fi
     '';
 
