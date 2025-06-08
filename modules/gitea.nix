aa5f41ca51ee88735f234c841f98deb5d3718519
Nico Felbinger <nico@felbinger.eu>
2025-06-08 16:31:07 +0200

    gitea: restructure

--- modules/gitea.nix
+++ modules/gitea.nix
@@ -3,164 +3,196 @@
   lib,
   ...
 }:
+let
+  cfg = config.secshell.gitea;
+  inherit (lib)
+    mkIf
+    types
+    mkEnableOption
+    mkOption
+    mkMerge
+    ;
+in
 {
   options.secshell.gitea = {
-    enable = lib.mkEnableOption "gitea";
-    domain = lib.mkOption {
-      type = lib.types.str;
+    enable = mkEnableOption "gitea";
+    domain = mkOption {
+      type = types.str;
       default = "git.${toString config.networking.fqdn}";
       defaultText = "git.\${toString config.networking.fqdn}";
     };
-    internal_port = lib.mkOption { type = lib.types.port; };
-    useLocalDatabase = lib.mkOption {
-      type = lib.types.bool;
+    internal_port = mkOption { type = types.port; };
+    useLocalDatabase = mkOption {
+      type = types.bool;
       default = true;
     };
     smtp = {
-      hostname = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
+      hostname = mkOption {
+        type = types.nullOr types.str;
         default = null;
       };
-      from = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
+      from = mkOption {
+        type = types.nullOr types.str;
         default = null;
       };
-      port = lib.mkOption {
-        type = lib.types.port;
+      port = mkOption {
+        type = types.port;
         default = 587;
       };
-      user = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
+      user = mkOption {
+        type = types.nullOr types.str;
         default = null;
       };
-      noReplyAddress = lib.mkOption {
-        type = lib.types.nullOr lib.types.str;
+      noReplyAddress = mkOption {
+        type = types.nullOr types.str;
         default = config.secshell.gitea.from;
         defaultText = "config.secshell.gitea.from";
       };
     };
     database = {
-      hostname = lib.mkOption {
-        type = lib.types.str;
+      hostname = mkOption {
+        type = types.str;
         default = "";
       };
-      username = lib.mkOption {
-        type = lib.types.str;
+      username = mkOption {
+        type = types.str;
         default = "gitea";
       };
-      name = lib.mkOption {
-        type = lib.types.str;
+      name = mkOption {
+        type = types.str;
         default = "gitea";
       };
     };
-    sshPort = lib.mkOption {
-      type = lib.types.port;
+    appName = mkOption {
+      type = types.str;
+      default = "Secure Shell Networks: Gitea";
+    };
+    sshPort = mkOption {
+      type = types.port;
       default = 22;
     };
-    requireSignInView = lib.mkOption {
-      type = lib.types.bool;
+    requireSignInView = mkOption {
+      type = types.bool;
       default = true;
     };
-    enableNotifyMail = lib.mkOption {
-      type = lib.types.bool;
+    enableNotifyMail = mkOption {
+      type = types.bool;
       default = true;
     };
-    allowOnlyExternalRegistrations = lib.mkOption {
-      type = lib.types.bool;
+    allowOnlyExternalRegistrations = mkOption {
+      type = types.bool;
       default = true;
     };
-    defaultKeepEmailPrivate = lib.mkOption {
-      type = lib.types.bool;
+    defaultKeepEmailPrivate = mkOption {
+      type = types.bool;
       default = true;
     };
   };
-  config = lib.mkIf config.secshell.gitea.enable {
-    sops.secrets = (
-      lib.optionalAttrs (!config.secshell.gitea.useLocalDatabase) {
-        "gitea/databasePassword".owner = "gitea";
-      }
-      // lib.optionalAttrs (config.secshell.gitea.smtp.hostname != null) {
-        "gitea/smtpPassword".owner = "gitea";
-      }
-    );
 
-    services.postgresql = lib.mkIf config.secshell.gitea.useLocalDatabase {
-      enable = true;
-      ensureDatabases = [ "gitea" ];
-    };
-
-    services.gitea = {
-      enable = true;
-      database =
-        {
-          type = "postgres";
-        }
-        // (lib.optionalAttrs (config.secshell.gitea.useLocalDatabase) { host = "/run/postgresql"; })
-        // (lib.optionalAttrs (!config.secshell.gitea.useLocalDatabase) {
-          host = config.secshell.gitea.database.hostname;
-          user = config.secshell.gitea.database.username;
-          name = config.secshell.gitea.database.name;
-          passwordFile = config.sops.secrets."gitea/databasePassword".path;
-          createDatabase = false;
-        });
-      settings = {
-        server = {
-          HTTP_ADDR = "127.0.0.1";
-          HTTP_PORT = config.secshell.gitea.internal_port;
-          DOMAIN = config.secshell.gitea.domain;
-          ROOT_URL = "https://${config.secshell.gitea.domain}";
-          SSH_PORT = config.secshell.gitea.sshPort;
-        };
-        service = {
-          "REQUIRE_SIGNIN_VIEW" = config.secshell.gitea.requireSignInView;
-          "ENABLE_NOTIFY_MAIL" = config.secshell.gitea.enableNotifyMail;
-          "ALLOW_ONLY_EXTERNAL_REGISTRATION" = config.secshell.gitea.allowOnlyExternalRegistrations;
-          "DEFAULT_KEEP_EMAIL_PRIVATE" = config.secshell.gitea.defaultKeepEmailPrivate;
-          "NO_REPLY_ADDRESS" = lib.mkIf (config.secshell.gitea.smtp.hostname != null) config.secshell.gitea.smtp.noReplyAddress;
-        };
-        mailer = lib.mkIf (config.secshell.gitea.smtp.hostname != null) {
-          ENABLED = true;
-          SMTP_ADDR = config.secshell.gitea.smtp.hostname;
-          SMTP_PORT = config.secshell.gitea.smtp.port;
-          FROM = config.secshell.gitea.smtp.from;
-          USER = config.secshell.gitea.smtp.user;
-        };
-        openid = {
-          ENABLE_OPENID_SIGNIN = false;
-          ENABLE_OPENID_SIGNUP = false;
-        };
-        oauth2_client = {
-          ENABLE_AUTO_REGISTRATION = true;
-        };
-        ui = {
-          SHOW_USER_EMAIL = false;
-          DEFAULT_SHOW_FULL_NAME = false;
-          EXPLORE_PAGING_NUM = 100;
-          ISSUE_PAGING_NUM = 100;
-        };
-      };
-      mailerPasswordFile = lib.mkIf (
-        config.secshell.gitea.smtp.hostname != null
-      ) config.sops.secrets."gitea/smtpPassword".path;
-    };
-    networking.firewall.allowedTCPPorts = [ config.secshell.gitea.sshPort ];
-
-    services.nginx = {
-      enable = true;
-      virtualHosts."${toString config.secshell.gitea.domain}" = {
-        locations = {
-          "/" = {
-            proxyPass = "http://127.0.0.1:${toString config.secshell.gitea.internal_port}/";
-            proxyWebsockets = true;
+  config = mkIf cfg.enable (mkMerge [
+    # base
+    {
+      services = {
+        gitea = {
+          enable = true;
+          inherit (cfg) appName;
+          settings = {
+            server = {
+              HTTP_ADDR = "127.0.0.1";
+              HTTP_PORT = config.secshell.gitea.internal_port;
+              DOMAIN = config.secshell.gitea.domain;
+              ROOT_URL = "https://${config.secshell.gitea.domain}";
+              SSH_PORT = config.secshell.gitea.sshPort;
+            };
+            service = {
+              "REQUIRE_SIGNIN_VIEW" = config.secshell.gitea.requireSignInView;
+              "ENABLE_NOTIFY_MAIL" = config.secshell.gitea.enableNotifyMail;
+              "ALLOW_ONLY_EXTERNAL_REGISTRATION" = cfg.allowOnlyExternalRegistrations;
+              "DEFAULT_KEEP_EMAIL_PRIVATE" = cfg.defaultKeepEmailPrivate;
+            };
+            openid = {
+              ENABLE_OPENID_SIGNIN = false;
+              ENABLE_OPENID_SIGNUP = false;
+            };
+            oauth2_client = {
+              ENABLE_AUTO_REGISTRATION = true;
+            };
+            ui = {
+              SHOW_USER_EMAIL = false;
+              DEFAULT_SHOW_FULL_NAME = false;
+              EXPLORE_PAGING_NUM = 100;
+              ISSUE_PAGING_NUM = 100;
+            };
           };
         };
-        serverName = toString config.secshell.gitea.domain;
+        nginx = {
+          enable = true;
+          virtualHosts."${toString cfg.domain}" = {
+            locations = {
+              "/" = {
+                proxyPass = "http://${config.services.gitea.settings.server.HTTP_ADDR}:${toString cfg.internal_port}/";
+                proxyWebsockets = true;
+              };
+            };
+            serverName = toString cfg.domain;
 
-        # use ACME DNS-01 challenge
-        useACMEHost = toString config.secshell.gitea.domain;
-        forceSSL = true;
+            # use ACME DNS-01 challenge
+            useACMEHost = toString cfg.domain;
+            forceSSL = true;
+          };
+        };
       };
-    };
-    security.acme.certs."${toString config.secshell.gitea.domain}" = { };
-  };
+
+      networking.firewall.allowedTCPPorts = [ cfg.sshPort ];
+
+      security.acme.certs."${toString cfg.domain}" = { };
+    }
+
+    # configure local database
+    (mkIf cfg.useLocalDatabase {
+      services = {
+        gitea.database = {
+          type = "postgres";
+          host = "/run/postgresql";
+        };
+        postgresql = {
+          enable = true;
+          ensureDatabases = [ "gitea" ];
+        };
+      };
+    })
+
+    # configure external database
+    (mkIf (!cfg.useLocalDatabase) {
+      sops.secrets."gitea/databasePassword".owner = "gitea";
+
+      services.gitea.database = {
+        type = "postgres";
+        host = cfg.database.hostname;
+        user = cfg.database.username;
+        name = cfg.database.name;
+        passwordFile = config.sops.secrets."gitea/databasePassword".path;
+        createDatabase = false;
+      };
+    })
+
+    # configure smtp
+    (mkIf (cfg.smtp.hostname != null) {
+      sops.secrets."gitea/smtpPassword".owner = "gitea";
+
+      services.gitea = {
+        settings = {
+          mailer = {
+            ENABLED = true;
+            SMTP_ADDR = cfg.smtp.hostname;
+            SMTP_PORT = cfg.smtp.port;
+            FROM = cfg.smtp.from;
+            USER = cfg.smtp.user;
+          };
+          service."NO_REPLY_ADDRESS" = cfg.smtp.noReplyAddress;
+        };
+        mailerPasswordFile = config.sops.secrets."gitea/smtpPassword".path;
+      };
+    })
+  ]);
 }
