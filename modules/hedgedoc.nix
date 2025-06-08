9014a637afd2e4910f54e31bdbe7ddc803b34d7f
Nico Felbinger <nico@felbinger.eu>
2025-06-08 16:56:02 +0200

    hedgedoc: restructure

--- modules/hedgedoc.nix
+++ modules/hedgedoc.nix
@@ -1,131 +1,155 @@
 { config, lib, ... }:
+let
+  cfg = config.secshell.hedgedoc;
+  inherit (lib)
+    mkIf
+    types
+    mkEnableOption
+    mkOption
+    mkMerge
+    mkAfter
+    ;
+in
 {
   options.secshell.hedgedoc = {
-    enable = lib.mkEnableOption "hedgedoc";
-    domain = lib.mkOption {
-      type = lib.types.str;
+    enable = mkEnableOption "hedgedoc";
+    domain = mkOption {
+      type = types.str;
       default = "md.${toString config.networking.fqdn}";
       defaultText = "md.\${toString config.networking.fqdn}";
     };
-    internal_port = lib.mkOption { type = lib.types.port; };
-    useLocalDatabase = lib.mkOption {
-      type = lib.types.bool;
+    internal_port = mkOption { type = types.port; };
+    useLocalDatabase = mkOption {
+      type = types.bool;
       default = true;
     };
     database = {
-      hostname = lib.mkOption {
-        type = lib.types.str;
+      hostname = mkOption {
+        type = types.str;
         default = "/run/postgresql";
       };
-      username = lib.mkOption {
-        type = lib.types.str;
+      username = mkOption {
+        type = types.str;
         default = "hedgedoc";
       };
-      name = lib.mkOption {
-        type = lib.types.str;
+      name = mkOption {
+        type = types.str;
         default = "hedgedoc";
       };
     };
     oidc = {
-      domain = lib.mkOption {
-        type = lib.types.str;
+      domain = mkOption {
+        type = types.str;
         default = "";
       };
-      realm = lib.mkOption {
-        type = lib.types.str;
+      realm = mkOption {
+        type = types.str;
         default = "main";
       };
-      clientId = lib.mkOption {
-        type = lib.types.str;
-        default = config.secshell.hedgedoc.domain;
+      clientId = mkOption {
+        type = types.str;
+        default = cfg.domain;
         defaultText = "config.secshell.hedgedoc.domain";
       };
     };
   };
-  config = lib.mkIf config.secshell.hedgedoc.enable {
-    sops = {
-      secrets =
-        {
-          "hedgedoc/sessionSecret" = { };
-        }
-        // (lib.optionalAttrs (!config.secshell.hedgedoc.useLocalDatabase) {
-          "hedgedoc/databasePassword" = { };
-        })
-        // (lib.optionalAttrs (config.secshell.hedgedoc.oidc.domain != "") {
-          "hedgedoc/oidcSecret" = { };
-        });
 
-      templates."hedgedoc/environment".content = ''
-        CMD_DB_PASSWORD=\"${config.sops.placeholder."hedgedoc/sessionSecret"}\"
-        ${lib.optionalString (!config.secshell.hedgedoc.useLocalDatabase)
-          "CMD_DB_PASSWORD=\"${config.sops.placeholder."hedgedoc/databasePassword"}\""
-        }
-        ${lib.optionalString (config.secshell.hedgedoc.oidc.domain != "")
-          "CMD_OAUTH2_CLIENT_SECRET=\"${config.sops.placeholder."hedgedoc/oidcSecret"}\""
-        }
-      '';
-    };
-
-    services.postgresql = lib.mkIf config.secshell.hedgedoc.useLocalDatabase {
-      enable = true;
-      ensureDatabases = [ "hedgedoc" ];
-    };
-
-    services.hedgedoc = {
-      enable = true;
-      environmentFile = config.sops.templates."hedgedoc/environment".path;
-      settings = {
-        domain = config.secshell.hedgedoc.domain;
-        host = "127.0.0.1";
-        port = config.secshell.hedgedoc.internal_port;
-        protocolUseSSL = true;
-
-        db = {
-          dialect = "postgresql";
-          host = config.secshell.hedgedoc.database.hostname;
-          username = config.secshell.hedgedoc.database.username;
-          database = config.secshell.hedgedoc.database.name;
-        };
-
-        email = config.secshell.hedgedoc.oidc.domain == "";
-        allowEmailRegister = false;
-
-        oauth2 = lib.mkIf (config.secshell.hedgedoc.oidc.domain != "") {
-          userProfileURL = "https://${config.secshell.hedgedoc.oidc.domain}/realms/${config.secshell.hedgedoc.oidc.realm}/protocol/openid-connect/userinfo";
-          userProfileUsernameAttr = "preferred_username";
-          userProfileDisplayNameAttr = "name";
-          userProfileEmailAttr = "email";
-          tokenURL = "https://${config.secshell.hedgedoc.oidc.domain}/realms/${config.secshell.hedgedoc.oidc.realm}/protocol/openid-connect/token";
-          authorizationURL = "https://${config.secshell.hedgedoc.oidc.domain}/realms/${config.secshell.hedgedoc.oidc.realm}/protocol/openid-connect/auth";
-          clientID = config.secshell.hedgedoc.oidc.clientId;
-          clientSecret = ""; # defined in secrets, but needs to exists for login button to show
-          providerName = "Keycloak";
-          scope = "openid email profile";
-        };
-
-        allowAnonymous = false;
-        allowAnonymousEdits = true;
-        allowFreeURL = true;
-        requireFreeURLAuthentication = true;
-        defaultPermission = "private";
+  config = mkIf cfg.enable (mkMerge [
+    # base
+    {
+      sops = {
+        secrets."hedgedoc/sessionSecret" = { };
+        templates."hedgedoc/environment".content = ''
+          CMD_SESSION_SECRET=\"${config.sops.placeholder."hedgedoc/sessionSecret"}\"
+        '';
       };
-    };
 
-    services.nginx = {
-      enable = true;
-      virtualHosts.${toString config.secshell.hedgedoc.domain} = {
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:${toString config.secshell.hedgedoc.internal_port}";
-          proxyWebsockets = true;
+      services = {
+        hedgedoc = {
+          enable = true;
+          environmentFile = config.sops.templates."hedgedoc/environment".path;
+          settings = {
+            domain = cfg.domain;
+            host = "127.0.0.1";
+            port = cfg.internal_port;
+            protocolUseSSL = true;
+
+            db = {
+              dialect = "postgresql";
+              host = cfg.database.hostname;
+              username = cfg.database.username;
+              database = cfg.database.name;
+            };
+
+            email = cfg.oidc.domain == "";
+            allowEmailRegister = false;
+
+            allowAnonymous = false;
+            allowAnonymousEdits = true;
+            allowFreeURL = true;
+            requireFreeURLAuthentication = true;
+            defaultPermission = "private";
+          };
         };
-        serverName = toString config.secshell.hedgedoc.domain;
 
-        # use ACME DNS-01 challenge
-        useACMEHost = toString config.secshell.hedgedoc.domain;
-        forceSSL = true;
+        nginx = {
+          enable = true;
+          virtualHosts.${toString cfg.domain} = {
+            locations."/" = {
+              proxyPass = "http://${config.services.hedgedoc.settings.host}:${toString cfg.internal_port}";
+              proxyWebsockets = true;
+            };
+            serverName = toString cfg.domain;
+
+            # use ACME DNS-01 challenge
+            useACMEHost = toString cfg.domain;
+            forceSSL = true;
+          };
+        };
       };
-    };
 
-    security.acme.certs."${toString config.secshell.hedgedoc.domain}" = { };
-  };
+      security.acme.certs."${toString cfg.domain}" = { };
+    }
+
+    # configure local database
+    (mkIf cfg.useLocalDatabase {
+      services.postgresql = {
+        enable = true;
+        ensureDatabases = [ "hedgedoc" ];
+      };
+    })
+
+    # configure external database
+    (mkIf (!cfg.useLocalDatabase) {
+      sops = {
+        secrets."hedgedoc/databasePassword" = { };
+        templates."hedgedoc/environment".content = mkAfter ''
+          CMD_DB_PASSWORD=\"${config.sops.placeholder."hedgedoc/databasePassword"}\"
+        '';
+      };
+    })
+
+    # configure oidc
+    (mkIf (cfg.oidc.domain != "") {
+      sops = {
+        secrets."hedgedoc/oidcSecret" = { };
+        templates."hedgedoc/environment".content = mkAfter ''
+          CMD_OAUTH2_CLIENT_SECRET=\"${config.sops.placeholder."hedgedoc/oidcSecret"}\"
+        '';
+      };
+
+      services.hedgedoc.settings.oauth2 = mkIf (cfg.oidc.domain != "") {
+        userProfileURL = "https://${cfg.oidc.domain}/realms/${cfg.oidc.realm}/protocol/openid-connect/userinfo";
+        userProfileUsernameAttr = "preferred_username";
+        userProfileDisplayNameAttr = "name";
+        userProfileEmailAttr = "email";
+        tokenURL = "https://${cfg.oidc.domain}/realms/${cfg.oidc.realm}/protocol/openid-connect/token";
+        authorizationURL = "https://${cfg.oidc.domain}/realms/${cfg.oidc.realm}/protocol/openid-connect/auth";
+        clientID = cfg.oidc.clientId;
+        clientSecret = ""; # defined in secrets, but needs to exists for login button to show
+        providerName = "Keycloak";
+        scope = "openid email profile";
+      };
+    })
+  ]);
 }
